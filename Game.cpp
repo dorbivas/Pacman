2a00cf37fe41244930ef14494c499c7a718fa13d
Dor Bivas <dorbi@mta.ac.il>
2021-11-14 21:40:27 +0200

    teleport + new board + directions and ghosts init

--- Game.cpp
+++ Game.cpp
@@ -39,7 +39,6 @@ void Game::boarders()
 	}
 }
 
-
 bool Game::is_valid_key(char c)
 {
 	if (c == 'a' || c == 'd' || c == 'w' || c == 's' || c == 'x' || c == 'A' || c == 'D' || c == 'W' || c == 'X' || c == 'S'|| c == ESC||c == 9)
@@ -47,7 +46,6 @@ bool Game::is_valid_key(char c)
 
 	return false;
 }
-
 
 void Game::Menu()
 {
@@ -84,14 +82,12 @@ void Game::Menu()
 
 	} while (userChoice != 0);
 	system("cls");
-	//Game g;
-	//g.board_init(); 
 }
 
-void Game::move(Pacman& Pacman, int dir_x, int dir_y)// Pos* food) intreract with food TODO
+void Game::move(Pacman& pacman, int dir_x, int dir_y)// Pos* food) intreract with food TODO
 {
-	int x = Pacman.get_position().get_x() + dir_x;
-	int y = Pacman.get_position().get_y() + dir_y;
+	int x = pacman.get_position().get_x() + dir_x;
+	int y = pacman.get_position().get_y() + dir_y;
 	Position nextPos(x, y); //holding cordinate the snake heading to
 
 	//if (if_move_towards_ghost(ghost.pos, const nextPos) || if_move_towards_wall(const nextPos)) //losing conditaions
@@ -103,9 +99,19 @@ void Game::move(Pacman& Pacman, int dir_x, int dir_y)// Pos* food) intreract wit
 		//if (ifAteFood)
 			//FoodInteract(snake, food);
 
-	Pacman.set_position(nextPos);
-}
+	if (is_teleporting(pacman.get_position()))
+		teleport(pacman);
 
+	else if (is_collided(pacman))//with ghost
+	{
+		pacman.decrease_soul();
+		//pacman.set_position(inital);
+		//currentKey = stay_upper_case;
+	}
+
+	else
+		pacman.set_position(nextPos);
+}
 
 void Game::game()
 {
@@ -116,98 +122,78 @@ void Game::game()
 	Pacman.set_souls(3);
 	Pacman.set_position(inital);
 	char currentKey;
-	//Pos* food = (Pos*)malloc(sizeof(Pos));
 
 	currentKey = _kbhit();
 	while (!is_valid_key(currentKey))
 		currentKey = _getch();
 	char temp = currentKey;
-	//Initiate(snake);
-	//FoodSpread(snake, food);
-	//Pacman.get_position().set_xy(INITIAL_X, INITIAL_Y);
-	//gotoxy(food->x, food->y);
-	//printf("\x1b[31m$"); //first food
 
 	while (currentKey != '9') //&& win condition)
 	{
 		int dir_x = 0, dir_y = 0;   //holding the directions
-		Sleep(100);			//1 second between moves
-		if (_kbhit())		// if any key was hit , only if a key was hit we read what key code it was
+		Sleep(400);            //1 second between moves
+		if (_kbhit())        // if any key was hit , only if a key was hit we read what key code it was
 			currentKey = _getch();
 		if (is_valid_key(currentKey)) //TODO FIX other keys dont need to stop Pacman
 		{
 			switch (currentKey)
 			{
-				case right_lower_case:
-				case right_upper_case:
-					if (!this->right_hit)
-					{
-						dir_x = 1;
-						dir_y = 0;
-					}
-					else
-						currentKey = stay_lower_case;
-					break;
-
-				case left_lower_case:
-				case left_upper_case:
-					if (!this->left_hit)
-					{
-						dir_x = -1;
-						dir_y = 0;
-					}
-					else
-						currentKey = stay_lower_case;
-					break;
-				case up_lower_case:
-				case up_upper_case:
-					if (!this->top_hit)
-					{
-						dir_x = 0;
-						dir_y = -1;
-					}
-					else
-						currentKey = stay_lower_case;
-					break;
-				case down_lower_case:
-				case down_upper_case:
-					if (!this->bottom_hit)
-					{
-						dir_x = 0;
-						dir_y = 1;
-					}
-					else
-						currentKey = stay_lower_case;
-					break;
-				case stay_lower_case:
-				case stay_upper_case:
-					dir_x = 0;
+			case right_lower_case:
+			case right_upper_case:
+				if (!this->right_hit)
+				{
+					dir_x = 1;
 					dir_y = 0;
-					break;
-				case ESC://PAUSE
-					cout << "pause" << endl;//fflush
-					char c = _getch();
-					while (c != ESC)
-					{
-						c = NULL;
-						c = _getch();
-					}
-					currentKey = temp;	
+				}
+				else
+					currentKey = stay_lower_case;
+				break;
+
+			case left_lower_case:
+			case left_upper_case:
+				if (!this->left_hit)
+				{
+					dir_x = -1;
+					dir_y = 0;
+				}
+				else
+					currentKey = stay_lower_case;
+				break;
+			case up_lower_case:
+			case up_upper_case:
+				if (!this->top_hit)
+				{
+					dir_x = 0;
+					dir_y = -1;
+				}
+				else
+					currentKey = stay_lower_case;
+				break;
+			case down_lower_case:
+			case down_upper_case:
+				if (!this->bottom_hit)
+				{
+					dir_x = 0;
+					dir_y = 1;
+				}
+				else
+					currentKey = stay_lower_case;
+				break;
+			case stay_lower_case:
+			case stay_upper_case:
+				dir_x = 0;
+				dir_y = 0;
+				break;
+			case ESC://PAUSE
+				pause();
+				currentKey = temp;
 			}
 			temp = currentKey;
 		}
 		move(Pacman, dir_x, dir_y);//food TODO
 		print_move(Pacman.get_position());
-		/*if (Pacman.get_position().get_y() == HIGHT)
-			this->top_hit = true;
-		if (Pacman.get_position().get_x() == WIDTH)
-			this->right_hit = true;*/
-		if (is_collided(Pacman))//with ghost
-		{
-			Pacman.decrease_soul();
-			Pacman.set_position(inital);
-			currentKey = stay_upper_case;
-		}
+	
+	
 		if (Pacman.get_souls() == 0)
 		{
 			system("cls");
@@ -218,34 +204,78 @@ void Game::game()
 	}
 	system("cls");
 }
-bool Game::is_collided(Pacman& Pacman)//with ghost
+bool Game::is_collided(Pacman& Pacman)//with ghost TODO
 {
 	int Pacman_x = Pacman.get_position().get_x();
 	int Pacman_y = Pacman.get_position().get_y();
-	if (Pacman_x == WIDTH + 14 || Pacman_x == 17 || Pacman_y == HIGHT || Pacman_y == 3)
-	{
-		//if (!is_telepoting(Pacman))
-			return true;
-	}
+	if (Pacman_x == WIDTH + 14  || Pacman_x == 17 || Pacman_y == HIGHT || Pacman_y == 3)
+		return true;
+	
 	return false;
 }
 
-bool Game::is_telepoting(Pacman& Pacman)//with ghost
+bool Game::is_teleporting(Position curr_pos)//teleporting the pacman and return if teleported
 {
-	int Pacman_x = Pacman.get_position().get_x();
-	int Pacman_y = Pacman.get_position().get_y();
+	int Pacman_x = curr_pos.get_x();
+	int Pacman_y = curr_pos.get_y();
 	
 	for (int i = 0; i < 3; i++) //bot and top teleports size is 3 blocks
 	{
 		if (Pacman_x == 53 + i && Pacman_y == HIGHT || Pacman_x == 53 + i && Pacman_y == 3)
-			return true;
+			return true;	
 	}
-	
-	if (Pacman_y == 15 && Pacman_x == 17 || Pacman_x == WIDTH + 14)
+	if (Pacman_y == 14 && Pacman_x == 17 || Pacman_x == WIDTH + 14)
 		return true;
 
 	return false;
+}
 
+
+void Game::teleport(Pacman& pacman)//teleporting the pacman and return if teleported
+{
+	int Pacman_x = pacman.get_position().get_x();
+	int Pacman_y = pacman.get_position().get_y();
+
+	for (int i = 0; i < 3; i++) //bot and top teleports size is 3 blocks
+	{
+		if (Pacman_x == 53 + i && Pacman_y == HIGHT)
+		{
+			pacman.set_position(Pacman_x, 3);
+			//pacman.set_direction(DOWN) TODO 
+		}
+			
+		if (Pacman_x == 53 + i && Pacman_y == 3)
+		{
+			pacman.set_position(Pacman_x, HIGHT);
+			//pacman.set_direction(UP) TODO 
+		}
+	}
+
+	if (Pacman_y == 14 && Pacman_x == 17)
+	{
+		pacman.set_position(WIDTH + 14, Pacman_y);
+		//pacman.set_direction(left) TODO 
+	}
+	if (Pacman_y == 14 && Pacman_x == WIDTH + 14)
+	{
+		pacman.set_position(17, Pacman_y);
+		//pacman.set_direction(right) TODO 
+	}
+}
+
+void Game::pause()
+{
+	Position pos;
+	pos.set_xy(0, 0);
+	print_move(pos);
+	cout << "\rPAUSE";
+	char c = _getch();
+	while (c != ESC)
+	{
+		c = NULL;
+		c = _getch();
+	}
+	cout << "\r     ";
 }
 
 void Game::print_move(Position pos) //displaying snake

ce0d3b7b27afdd11078f0ca6236f2a5a3c57a042
Yarden Agami <yardenagami@gmail.com>
2022-01-02 17:50:28 +0200

    new99876

--- Main.cpp
+++ Main.cpp
@@ -2,17 +2,16 @@
 int main(int argc, char** argv) {
 	cursor_visibility(false);
 	Game game;
+	string str;
 
 	if (argc == 1)
 		game.run_menu();
 
 	else if (argc == 2)
 	{
-		string str = argv[1];
+		str = argv[1];
 		if (str.compare("-load")==0)
 			game.run_load();
-		else if (str.compare("-silent")==0)
-			game.run_silent();
 		else if (str.compare("-save")==0)
 		{
 			game.set_save_mode(true);
@@ -24,10 +23,26 @@ int main(int argc, char** argv) {
 			cout << "ERROR" << endl;
 		}
 	}
+	else if (argc == 3)
+	{
+		str = argv[1];
+		if (str.compare("-load") == 0 /*|| str.compare("-save") == 0*/)
+		{
+			str = argv[2];
+			if (str.compare("-silent") == 0)
+				game.run_silent();
+			else
+				cout << "ERROR";//TODO EXCEPTIONS (not silent)
+		}
+		else
+			cout << "error";//TODO EXCEPTIONS (notload)
+
+	}
 	else
+	{
 		game.run_menu();//for testing without cmd
 		//throw(dor)-todo
 		cout << "ERROR" << endl;
-
+	}
 	return 0; 
 }

68344f7a006f5f7c4474771fffbb76f25935dd0d
Dor Bivas <dorbi@mta.ac.il>
2022-01-05 13:30:47 +0200

    souls = 9

--- Pacman.h
+++ Pacman.h
@@ -9,14 +9,14 @@ private:
 	int souls = STARTING_SOULS;
 	int score = ZERO_POINTS;
 	int total_steps = ZERO_STEPS;
-	
+
 public:
-	enum pacman_table { STARTING_SOULS = 30, ZERO_POINTS = 0, ZERO_STEPS=0};
+	enum pacman_table { STARTING_SOULS = 9, ZERO_POINTS = 0, ZERO_STEPS = 0 };
 	//--Constructor --//
 	Pacman() {
 		shape = Shape::PACMAN;
 		color = Board::Color::YELLOW;
-		direction = (int)Direction::STAY;		
+		direction = (int)Direction::STAY;
 	};
 	Pacman(int souls, int direction, Position& pos, int score);
 
@@ -29,7 +29,7 @@ public:
 	int get_total_steps() { return total_steps; }
 
 	void add_score(int new_val);
-	void init_score(){score = ZERO_POINTS;}
+	void init_score() { score = ZERO_POINTS; }
 	void init_total_steps() { total_steps = ZERO_STEPS; }
 	void decrease_soul();
 	bool is_invalid_place(const Position& next_pos);
